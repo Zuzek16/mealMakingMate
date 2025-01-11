0d24643ca02aaba23deca54d5ccbe243b65f34e0
Zuzek16 <zuzekzych@gmail.com>
2025-01-11 17:24:20 +0100

    readME and init files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Meal Making Mate
+
+Ideas for meals are running out? Hoping to try something new?
+
+Creating a meal for everyday doesn't have to be a chore! Try this generator of meals to inspire yourself or maybe dare to try a new combination?
+
+## Technologies
+JavaScript
---
+++ makeMeal.js
@@ -0,0 +1,5 @@
+const carbs = ["rice noodles", "pasta", "rice", "cereal", "bread", "oats", "red beans"];
+const protein = ["chickpeas", "chicken", "beef", "pork", "eggs", "lentils", "cottage cheese", "greek joghurt", "natural joghurt", "turkey", "joghurt", "salmon", "cod", "tuna", "mackerel", "black beans"];
+const vegetables = ["potoato", "cucumber", "paprika", "tomato", "brussels", "sweet potato", "broccoli", "peas", "carrot", "lettuce", "celery", "califlower"];
+const fruit = ["apple", "pear", "kiwi", "berries", "pineapple", "banana", "corn", "orange", "lemon", "grapes", "cherries"];
+const drinks = ["water", "milk", " cocoa", "lemonade", "sparkling water", "tea", "coffee", "mocha", "fizzy drink"];
