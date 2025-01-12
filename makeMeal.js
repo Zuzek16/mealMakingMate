702edb80f50dc596ec02dae5b357b39c5a461382
Zuzek16 <zuzekzych@gmail.com>
2025-01-12 21:54:47 +0100

    added gallery; no inputs checked special message

--- makeMeal.js
+++ makeMeal.js
@@ -1,11 +1,12 @@
-const carbs = ["rice noodles", "pasta", "rice", "cereal", "bread", "oats", "red beans"];
-const protein = ["chickpeas", "chicken", "beef", "pork", "eggs", "lentils", "cottage cheese", "greek joghurt", "natural joghurt", "turkey", "joghurt", "salmon", "cod", "tuna", "mackerel", "black beans"];
-const vegetables = ["potoato", "cucumber", "paprika", "tomato", "brussels", "sweet potato", "broccoli", "peas", "carrot", "lettuce", "celery", "califlower"];
-const fruit = ["apple", "pear", "kiwi", "berries", "pineapple", "banana", "corn", "orange", "lemon", "grapes", "cherries"];
+const carbs = ["rice noodles", "pasta", "rice", "cereal", "bread", "oats", "red beans", "tortilla"];
+const protein = ["schrimp", "chickpeas", "chicken", "beef", "pork", "eggs", "lentils", "cottage cheese", "greek joghurt", "natural joghurt", "turkey", "joghurt", "salmon", "cod", "tuna", "mackerel", "black beans"];
+const vegetables = ["potoato", "cabbage", "cucumber", "paprika", "tomato", "brussels", "sweet potato", "broccoli", "peas", "carrot", "lettuce", "celery", "califlower"];
+const fruit = ["apple", "melon", "watermelon", "pear", "kiwi", "berries", "pineapple", "banana", "corn", "orange", "lemon", "grapes", "cherries"];
 const drinks = ["water", "milk", "fruit juice", "smoothie", "milkshake", " cocoa", "lemonade", "sparkling water", "tea", "coffee", "mocha", "fizzy drink"];
 
 let allOptions = [carbs, protein, vegetables, fruit, drinks];
 let allOptionsNames = ["Carbs", "Protein", "Vegetables", "Fruit", "Drink"];
+let noneChecked = false;
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomIntInclusive(min, max) {
@@ -25,15 +26,19 @@ function genMeal(random) {
         });
     } else {
         const allInputs = document.querySelectorAll("input[type=checkbox]");
-        
+        noneChecked = true;
         Array.from(allInputs).forEach((input, id)  => {
             if (input.checked) {
+                noneChecked = false;
                 console.log(input.checked);
                 let val = allOptions[id][getRandomIntInclusive(0, (allOptions[id].length - 1))]
                     
                 anwser += pStart + (input.name[0]).toUpperCase() + (input.name).slice(1) + ": " + val + pEnd; 
             }
         });
+    }
+    if (noneChecked) {
+        anwser = pStart + "No categories checked..." + pEnd;
     }
     document.getElementById("anwser").innerHTML = anwser;
 }
